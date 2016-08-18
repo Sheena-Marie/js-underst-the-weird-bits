8e520546187875b7703b43da173b598d80dbadb8
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-08-18 11:51:35 +1000

    finished next lesson on Hoisting

--- Starter/app.js
+++ Starter/app.js
@@ -1,5 +1,16 @@
+// b(); //this calls the function
+// console.log(a);
+
+// Trying the call function and the console.log at the top of the page.
+// You get 'undefined' for console.log(a);
+
 var a = 'Hello World';
 
 function b() {
-  
-};
+  console.log('Called b!');
+}
+
+// It's better to have these at the bottom of the page.
+
+b(); //this calls the function
+console.log(a);
