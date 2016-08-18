081797bd1aeeec7add65f5764ea416a55901d889
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-08-18 12:44:29 +1000

    understanding the difference between the creation and execution phase

--- Starter/app.js
+++ Starter/app.js
@@ -1,8 +1,11 @@
-var a = 'Hello World!';
+function b() {
+  console.log('called b!');
+}
+
+b();
+
 console.log(a);
 
-if (a === undefined) {
-  console.log('a is undefined!');
-} else {
-  console.log('a is defined!');
-}
+var a = 'Hello World!';
+
+console.log(a);
